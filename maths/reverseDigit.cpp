2a03b5cf4bb670f90cf32b1813bc495f9d527e66
vin3y <vinaysankar7@gmail.com>
2025-03-15 21:54:08 +0530

    reverse number leetcode 7 question 2 tuf a-z math series

---
+++ maths/reverseDigit.cpp
@@ -0,0 +1,42 @@
+//Leetcode no. 7 medium
+//Given a signed 32-bit integer x, return x with its digits reversed.
+//If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+//
+//Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+//
+//Example 1:
+//
+//Input: x = 123
+//Output: 321
+//Example 2:
+//
+//Input: x = -123
+//Output: -321
+//Example 3:
+//
+//Input: x = 120
+//Output: 21
+
+
+//Constraints:
+//-231 <= x <= 231 - 1
+
+
+
+#include <climits>
+
+class Solution {
+public:
+    int reverse(int x) {
+        int reversedNumber = 0;
+        while (x != 0) { //check if the number doesn't go underflow
+            if (reversedNumber < INT_MIN / 10 || reversedNumber > INT_MAX / 10) {
+                return 0;  //when multiplied* 10 whether it goes overflow or underflow
+            }
+            reversedNumber = reversedNumber * 10 + x % 10; //pop and add to the reversed digit
+            x /= 10;
+        }
+
+        return reversedNumber;  // Return the reversed number
+    }
+};
